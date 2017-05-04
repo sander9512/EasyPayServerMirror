52a36296eed3be721fdcc5b9c5411a4769a27924
Felix Boons <felixboons@hotmail.com>
2017-05-04 10:00:17 +0200

    connection_query_toegevoegd

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Felix on 3-5-2017.
+ */
+
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host : 'eu-cdbr-west-01.cleardb.com',
+    user : 'bf45754e8eb79a',
+    password : 'cc63c676',
+    database : 'heroku_05fdd2a232b52ba'
+});
+
+connection.connect();
+
+app.get('/', function(request, response) {
+    connection.query('SELECT * from t_users', function(err, rows, fields) {
+        if (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+        response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+    });
+});
+
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+    console.log("Listening on " + port + "...");
+});
