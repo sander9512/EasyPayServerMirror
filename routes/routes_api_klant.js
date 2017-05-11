54ee59caf058439c10613e701bf27e0649325b40
Bart int Veld <bveld1@avans.nl>
2017-05-11 11:56:05 +0200

    URL change + Password update

--- routes/routes_api_klant.js
+++ routes/routes_api_klant.js
@@ -47,12 +47,33 @@ router.get('/', function(req, res) {
     })
 });
 
-router.put('/id=:customerid?&saldo=:amount?&datum=:date?', function (req, res) {
+router.put('/id=:customerid?/saldo=:amount?&datum=:date?', function (req, res) {
     var customerid = req.params.customerid;
     var amount = req.params.amount;
     var date = req.params.date;
 
     var queryStr = "UPDATE klant SET Saldo = '" +amount+ "', TimeLog = '" +date+ "' WHERE KlantId=" +customerid;
+
+    connector.getConnection( function (err, connection) {
+        if (err){
+            console.log(err);
+        }else {
+            connection.query(queryStr, function (err, rows) {
+                if(err){
+                    console.log(err);
+                }else{
+                    res.send('UPDATED!')
+                }
+            })
+        }
+    })
+});
+
+router.put('/id=:customerid?/wachtwoord=:password?', function (req, res) {
+    var customerid = req.params.customerid;
+    var password = req.params.password;
+
+    var queryStr = "UPDATE klant SET Wachtwoord = '" + password + "' WHERE KlantId=" + customerid;
 
     connector.getConnection( function (err, connection) {
         if (err){
