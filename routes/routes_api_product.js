13d0f7f0dab1100b48e9cc689083bd8264e62be4
Jurian Vink <jhvink1@avans.nl>
2017-05-16 11:04:43 +0200

    Added_all_categories

--- routes/routes_api_product.js
+++ routes/routes_api_product.js
@@ -21,7 +21,43 @@ router.get('/food', function(req, res) {
     });
 });
 
-router.get('', function(req, res) {
+router.get('/drank', function(req, res) {
+    var queryStr = 'SELECT * from product WHERE Categorie = "Drank"'
+
+    connector.getConnection( function (err, connection) {
+        if(err){
+            console.log(err);
+        }else {
+            connection.query(queryStr, function (err, rows) {
+                if (err){
+                    console.log(err)
+                }else{
+                    res.status(200).json({"items" : rows});
+                }
+            });
+        }
+    });
+});
+
+router.get('/frisdrank', function(req, res) {
+    var queryStr = 'SELECT * from product WHERE Categorie = "Frisdrank"'
+
+    connector.getConnection( function (err, connection) {
+        if(err){
+            console.log(err);
+        }else {
+            connection.query(queryStr, function (err, rows) {
+                if (err){
+                    console.log(err)
+                }else{
+                    res.status(200).json({"items" : rows});
+                }
+            });
+        }
+    });
+});
+
+router.get('*', function(req, res) {
     var queryStr = 'SELECT * from product'
 
     connector.getConnection( function (err, connection) {
