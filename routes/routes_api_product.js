66c9a5144a077109024f35a2b6af51540c7f32d7
Jurian Vink <jhvink1@avans.nl>
2017-05-16 10:52:31 +0200

    Added_food_category

--- routes/routes_api_product.js
+++ routes/routes_api_product.js
@@ -3,6 +3,24 @@ var express = require('express');
 var router = express.Router();
 var connector = require('../db/db_connector');
 
+router.get('/food', function(req, res) {
+    var queryStr = 'SELECT * from product WHERE `Categorie`=`Eten`'
+
+    connector.getConnection( function (err, connection) {
+        if(err){
+            console.log(err);
+        }else {
+            connection.query(queryStr, function (err, rows) {
+                if (err){
+                    console.log(err)
+                }else{
+                    res.status(200).json({"items" : rows});
+                }
+            });
+        }
+    });
+});
+
 router.get('', function(req, res) {
     var queryStr = 'SELECT * from product'
 
