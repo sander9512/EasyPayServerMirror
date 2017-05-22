cc39d1d9e5a67391ef67fd7f099f136d5cdaff9f
Felix Boons <felixboons@hotmail.com>
2017-05-22 11:43:30 +0200

    Endpoint queries aangepast

--- routes/routes_api_bestelling.js
+++ routes/routes_api_bestelling.js
@@ -3,25 +3,72 @@ var express = require('express');
 var router = express.Router();
 var connector = require('../db/db_connector');
 
+//default get endpoint to show all orders, or a specific order when orderNumber is given
+router.get(':orderNumber?', function (req, res) {
 
-router.get('', function(req, res) {
+    var orderNumber = req.params.orderNumber;
+    var query = "";
 
-    var queryStr = 'SELECT * from bestelling';
+    if (orderNumber) {
+        query = "SELECT * from bestelling WHERE `bestellingNummer` = '" + orderNumber + "';";
+    } else {
+        query = 'SELECT * from bestelling';
+    }
 
-    connector.getConnection( function (err, connection) {
-        if (err){
+    connector.getConnection(function (err, connection) {
+        if (err) {
             console.log(err);
-        }else {
-            connection.query(queryStr, function (err, rows) {
+        } else {
+            connection.query(query, function (err, rows) {
                 connection.release();
-                if (err){
+                if (err) {
                     console.log(err);
-                }else {
-                    res.status(200).json({"items" : rows})
+                } else {
+                    res.status(200).json({"items": rows})
                 }
             })
         }
-    });
+    })
+});
+
+router.get('/check/available/ordernumber', function(req, res) {
+
+    var query = "SELECT bestellingNummer FROM bestelling ORDER BY bestellingNummer DESC LIMIT 1";
+
+    connector.getConnection(function (err, connection) {
+        if (err) {
+            console.log(err);
+        } else {
+            connection.query(query, function (err, rows) {
+                connection.release();
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.status(200).json({"items": rows})
+                }
+            })
+        }
+    })
+});
+
+router.put('/update/:orderNumber', function (req, res) {
+
+    var query = "UPDATE bestelling SET `status` = 'PAID' WHERE `bestellingNummer` =" + orderNumber + "';";
+
+    connector.getConnection(function (err, connection) {
+        if (err) {
+            console.log(err);
+        } else {
+            connection.query(query, function (err, rows) {
+                connection.release();
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.status(200).json({"items": rows})
+                }
+            })
+        }
+    })
 });
 
 module.exports = router;
