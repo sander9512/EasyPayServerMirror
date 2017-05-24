79933438bbc449d6573d6a5f0aa7b7527b5339e1
Felix Boons <felixboons@hotmail.com>
2017-05-24 12:19:07 +0200

    endpoint-for-order-creation-added

--- routes/routes_api_bestelling.js
+++ routes/routes_api_bestelling.js
@@ -31,6 +31,7 @@ router.get('/:orderNumber?', function (req, res) {
     })
 });
 
+//check for the first available 'bestellingNummer'. This is used to create a new unique order
 router.get('/check/available/ordernumber', function(req, res) {
 
     var query = "SELECT (bestellingNummer +1) AS nextAvailableOrderNumber FROM bestelling ORDER BY bestellingNummer DESC LIMIT 1";
@@ -51,6 +52,36 @@ router.get('/check/available/ordernumber', function(req, res) {
     })
 });
 
+//create new order feature
+router.put('/new/:klantId/:productId/:bestellingnummer', function(req, res) {
+
+
+    var klantId = req.params.klantId;
+    var productId = req.params.productId;
+    // var locatie = req.params.locatie;
+    // var prijs = req.params.prijs;
+    var bestellingnummer = req.params.bestellingnummer;
+
+    var query = "INSERT INTO bestelling (NULL, "+klantId+", "+productId+", 'WAITING', "+bestellingnummer+";";
+
+    connector.getConnection(function(err, connection) {
+        if (err) {
+            console.log(err);
+        } else {
+            connction.query(query, function(err, rows) {
+                connection.release();
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.status(200).json({"items": rows});
+                }
+            })
+        }
+    })
+});
+
+
+//update a specific order (with ordernnumer given as params) to status 'PAID'
 router.put('/update/:orderNumber', function (req, res) {
 
     var orderNumber = req.params.orderNumber;
