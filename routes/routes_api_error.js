b2cdb987767f21df6f0ddd4070035fb9a061e654
imottall <timobuitenhuis@gmail.com>
2017-05-24 09:43:56 +0200

    Fixed a error
    Missed the essential connector.getConnection line. In short big fckup from my side

--- routes/routes_api_error.js
+++ routes/routes_api_error.js
@@ -10,15 +10,21 @@ router.put('/add_error/:deviceSDK?/:device?/:model?/:product?/:error?', function
     var model = req.params.model || '';
     var product = req.params.product || '';
     var error  = req.params.error || '';
-
-    connection.query('INSERT INTO error_data(deviceSDK,device,model,product,error) VALUES( "' + [deviceSDK] + '","' + [device] + '","' + [model] + '","' + [product] + '","' + [error] + '");',
-        function (error, rows, fields) {
-            if (error) {
-                res.status(400).json(error);
-            } else {
-                res.status(200).json(rows);
-            }
-        });
+    
+    connector.getConnection(function (err, connection) {
+        if (err) {
+            console.log(err);
+        } else {
+            connection.query('INSERT INTO error_data(deviceSDK,device,model,product,error) VALUES( "' + [deviceSDK] + '","' + [device] + '","' + [model] + '","' + [product] + '","' + [error] + '");',
+                function (error, rows) {
+                    if (error) {
+                        res.status(400).json(error);
+                    } else {
+                        res.status(200).json(rows);
+                    }
+                });
+        }
+    })
 });
 
 module.exports = router;
