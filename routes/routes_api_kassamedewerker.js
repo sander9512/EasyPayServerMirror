e275aeef1b2b08e76b5e738b7e69482f6ad8adbe
imottall <timobuitenhuis@gmail.com>
2017-05-30 12:08:28 +0200

    Redit my earlier reverted commit
    Thought dezefelix had already fixed a issue because of a file that was in the android-project. Turned out this file wasn't newly created but was a copy from the easypay customer app.

--- routes/routes_api_kassamedewerker.js
+++ routes/routes_api_kassamedewerker.js
@@ -119,4 +119,31 @@ router.put('/id=:employeeid?/hours=:hoursWorked?', function (req, res) {
     })
 });
 
+//Get customer data
+router.get('/getcustomer/:customerid?', function (req, res) {
+    var cid = req.params.customerid || '';
+    var queryStr;
+
+    if (cid) {
+        queryStr = "SELECT voornaam, achternaam, KlantId FROM klant WHERE KlantId=" + cid + ";";
+
+        connector.getConnection(function (err, connection) {
+            if (err) {
+                console.log(err);
+            } else {
+                connection.query(queryStr, function (err, rows) {
+                    connection.release();
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.status(200).json(rows)
+                    }
+                })
+            }
+        })
+    } else {
+        res.status(404).send("Please put in a correct customer id")
+    }
+});
+
 module.exports = router;
