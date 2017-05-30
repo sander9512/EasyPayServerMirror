984a7b36f1f8a5c1035a503dd8f74c286733634b
Felix Boons <felixboons@hotmail.com>
2017-05-30 09:52:43 +0200

    login-feature-added-for-employee

--- routes/routes_api_kassamedewerker.js
+++ routes/routes_api_kassamedewerker.js
@@ -23,4 +23,31 @@ router.get('', function(req, res) {
     });
 });
 
+//login feature
+router.get('/login/:username?', function (req, res) {
+    var username = req.params.username || '';
+    var queryStr;
+
+    if (username) {
+        queryStr = "SELECT * from kassamedewerker WHERE `Gebruikersnaam` = '" + username + "'"
+
+        connector.getConnection(function (err, connection) {
+            if (err) {
+                console.log(err);
+            } else {
+                connection.query(queryStr, function (err, rows) {
+                    connection.release();
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.status(200).json({"items": rows})
+                    }
+                })
+            }
+        })
+    } else {
+        res.status(404).send("Please use api/klant/login/USERNAME")
+    }
+});
+
 module.exports = router;
